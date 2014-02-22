d5ea4e6ab95c22ac12b268526226db90e20ae3a1
AdamCmiel <adamcmiel@gmail.com>
2014-02-21 18:42:07 -0800

    can show nav view and schedule view

--- www/js/app.js
+++ www/js/app.js
@@ -3,8 +3,20 @@ var app;
 app = angular.module('EB', ['ngRoute', 'ionic']);
 
 app.config(function($routeProvider) {
-  $routeProvider.when('/', {
+  $routeProvider
+  .when('/login', {
     templateUrl: 'templates/login.html',
     controller: 'LoginController'
+  })
+  .when('/nav', {
+    templateUrl: 'templates/nav.html',
+    controller: 'NavController'
+  })
+  .when('/schedule', {
+    templateUrl: 'templates/schedule.html',
+    controller: 'ScheduleController'
+  })
+  .otherwise({
+    redirectTo: '/login'
   });
 });
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -3,7 +3,83 @@ var app;
 app = angular.module('EB');
 
 app.controller('LoginController', function($scope, $location) {
-  $scope.facebookLogin = function() {
-    console.log('facebookLogin');
-  };
+  $scope.facebookLogin = function(){
+    $location.path('/nav');
+  }
 });
+
+app.controller('NavController', function($scope, $location) {
+  $scope.logout = function(){
+    $location.path('/login');
+  }
+  $scope.redirect = function(path) {
+    $location.path('/' + path);
+  }
+  // $scope.watchVideos = function() {
+  //   $location.path('/videos')
+  // }
+  // $scope.bookMassage = function() {
+  //   $location.path('/massage')
+  // }
+  // $scope.donate = function() {
+  //   $location.path('/donate')
+  // }
+  // $scope.sendMessage = function() {
+  //   $location.path('/message')
+  // }
+  $scope.leftButtons = [
+    { 
+      type: 'button-positive',
+      content: '<i class="icon ion-android-hand"></i>',
+      tap: function(e) {
+        $scope.logout();
+      }
+    }
+  ];
+  $scope.navLinks = [
+    {
+      verb: "Yoga Schedule",
+      href: "schedule",
+      image: "schedule_icon"
+    },
+    {
+      verb: "Media",
+      href: "media",
+      image: "video_icon"
+    },
+    {
+      verb: "Massage",
+      href: "massage",
+      image: "massage_icon"
+    },
+    {
+      verb: "Donate",
+      href: "donate",
+      image: "donate_icon"
+    },
+    {
+      verb: "Message",
+      href: "message",
+      image: "message_icon"
+    }
+  ];
+});
+
+app.controller('ScheduleController', function($scope, $http, $location) {
+  $scope.getClasses = function(){
+    $http.get('http://energiesbalanced.herokuapp.com/api/yoga_classes')
+         .success(function(data){
+           $scope.classes = data;
+         })
+         .error(function(err){
+           console.log(err);
+         });
+  }
+
+  $scope.logout = function(){
+    $location.path('/login');
+  }
+
+  $scope.getClasses();
+})
+
